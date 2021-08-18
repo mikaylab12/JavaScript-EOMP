5aa0ca488f9cd8ce280b00a6114f1ede3a3a80f9
Mikayla Beelders <mikaylabeelders@gmail.com>
2021-08-18 22:31:29 +0200

    updates

--- scripts/add_product.js
+++ scripts/add_product.js
@@ -22,27 +22,6 @@ modalClose.addEventListener('click', function(){
     document.querySelectorAll('.product').forEach(product => product.style.zIndex = 0)
 })
 
-// function to add to cart 
-// function addToCart(){
-
-//     let carts = document.querySelector(".btn-cart");
-    
-// for(let i=0; i< carts.length; i++){
-//     carts[i].addEventListener('click', () => {
-//         console.log("Added to cart");
-//         cartNumbers();
-//     })
-// }
-
-// function cartNumbers() {
-//     let productNumbers = localStorage.getItem('cartNumbers');
-//     console.log(productNumbers)
-//     productNumbers = parseInt(productNumbers)
-//     localStorage.setItem('cartNumbers', 1);
-// }
-
-// }
-
 
 // function to show products 
 fetch('https://flask-project-eomp.herokuapp.com/show-products')
@@ -72,9 +51,7 @@ fetch('https://flask-project-eomp.herokuapp.com/show-products')
 
 // function to add to cart
 function addToCart(id){
-    // let quantity = e.target.parentNode.querySelector('.cartquantity').value
     let object = {}
-    // let productID = document.querySelector(".product_id").value
     console.log(id)
     fetch(`https://flask-project-eomp.herokuapp.com/view-product/${id}`, {
         headers: {
@@ -137,149 +114,6 @@ function addProduct(){
         alert("Product added successfully.")})
 }
 
-// update products
-// function updateProducts(){
-//     let productID = document.querySelector("#product-id").value
-//     fetch(`https://flask-project-eomp.herokuapp.com/edit-product/${productID}/`, {
-//         method: 'PUT',
-//         body: JSON.stringify({
-//             // "user_id": document.getElementById("user-id").value,
-//             "first_name": document.getElementById("name").value,
-//             "last_name": document.getElementById("surname").value,
-//             "email_address": document.getElementById("email").value,
-//             "username": document.getElementById("username").value,
-//             "password": document.getElementById("password").value,
-            
-//         }),
-//         headers: {
-//             'Content-Type' : 'application/json',
-//             // 'Authorization' : `jwt ${myStorage.getItem('jwt-token')}`
-//         },
-//     })
-//     .then(res => res.json())
-//     .then(res => {
-//         console.log(res);
-//         console.log(res["access_token"]);
-//         myStorage.setItem("jwt-token", res["access_token"]);
-//         alert("Product updated successfully.")})
-// }
-
-
-// function addClicked(id){
-    // let productID = document.querySelector("#delProduct-id").value
-    // fetch(`https://flask-project-eomp.herokuapp.com/view-product/${id}/`, {
-    //             method: "GET",
-    //             body: JSON.stringify(),
-    //             headers: {
-    //                 'Content-type': 'application/json',
-    //             }
-    //     })
-    //     .then(res => res.json())
-    //         .then(data => {
-    //             console.log(data)
-    //             console.log(data['data'][0])
-    //             let title = `${data['data'][1]}`
-    //             let price = `${data['data'][2]}`
-    //             let imageSrc = `${data['data'][5]}`
-    //             console.log(title)
-    //             console.log(price)
-    //             console.log(imageSrc)
-    //             addItemToCart(title, price, imageSrc)
-    //             cartTotal()
-    //     })
-        // document.getElementsByClassName('btn-cart1').addEventListener("click", addClicked())
-        // }
-
-
-// function addItemToCart(title, price, imageSrc){
-//     let cartRow = document.createElement('div')
-//     cartRow.classList.add('checkout-subheadings2')
-//     let cartItems = document.querySelector('#checkout-container')
-//     let cartItemName = cartItems.querySelector('.cart-item-title')
-//     console.log(cartItemName)
-//     for(let i = 0; i <  cartItemName.length; i++){
-//         if(cartItemName[i].innerText == title){
-//             alert("This item is already added to the cart")
-//             return
-//         }
-//     }
-//     let cartRowContent = `<div class="cart-item cart-column">
-//                           <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
-//                           <span class="cart-item-title">${title}</span>
-//                           </div>
-//                           <span class="cart-price cart-column">R${price}</span>
-//                           <div class="cart-quantity cart-column">
-//                           <input class="cart-quantity-input" type="number" value="1">
-//                         <button class="btn btn-danger" type="button">REMOVE</button>
-//                     </div>`
-    
-//     cartRow.innerHTML = cartRowContent
-//     cartItems.append(cartRow)
-    // cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeItem )
-    // cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChange)
- 
-// }
-
-
-//     let cartRowContent = `<div class="cart-item cart-column">
-//                           <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
-//                           <span class="cart-item-title">${title}</span>
-//                           </div>
-//                           <span class="cart-price cart-column">R${price}</span>
-//                           <div class="cart-quantity cart-column">
-//                           <input class="cart-quantity-input" type="number" value="1">
-//                         <button class="btn btn-danger" type="button">REMOVE</button>
-//                     </div>`
-    
-//     cartRow.innerHTML = cartRowContent
-
-// let itemId = idStorage.getItem('id')
-// // function to show products 
-// fetch(`https://flask-project-eomp.herokuapp.com/view-product/${itemId}`)
-// .then(res => res.json())
-// .then(data =>{
-//     console.log(data)
-
-
-    // let productContainer = document.querySelector('#product-container')
-    // productContainer.innerHTML = "";
-    // data['data'].forEach(product => {
-    //     productContainer.innerHTML += `<div class = 'product'>
-    //                                     <h1 class="product_id">${product[0]}.</h1>
-    //                                     <img src="${product[5]}" class="product_image">
-    //                                     <h4 class="product_title">${product[1]}</h4>
-    //                                     <p class="product_price">R ${product[2]}<p> 
-    //                                     <p class="product_description">${product[4]}<p>
-                                        
-    //                                     <div class="cart">
-    //                                     <button class="btn-cart1" onclick="addToCart(${product[0]})">ADD TO CART</button>
-    //                                     </div>
-                                        
-    //                                     </div>`
-// })
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // function to increase quantity
 function quantityChange(){
     let input = event.target
@@ -308,7 +142,7 @@ for(let i = 0; i < removeCart.length; i++){
 }
 }
 
-// function to calculate total amount : Tashwill
+// function to calculate total amount 
 function cartTotal(){
 let cartContainer = document.getElementsByClassName('cart-items')[0]
 let cartRow = cartContainer.getElementsByClassName('cart-row')
